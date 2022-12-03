99d6d9608eee11c245fcfec0b27aa29c32b701f7
Ian Waddington <iwaddo@icloud.com>
2022-12-03 07:50:53 +0000

    Initial Commit

---
+++ Days 32 2 34 Project 6/Days_32_2_34_Project_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Days_32_2_34_Project_6App.swift
+//  Days 32 2 34 Project 6
+//
+//  Created by Ian Waddington on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Days_32_2_34_Project_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
